b9930dd1220c15469e9b915cefcdc7877d2d198a
Herczku Misi <kmisku@gmail.com>
2016-04-16 13:48:27 +0200

    Add filter and default directory

--- M_CSHARP_TurtleGraphics/M_CSHARP_TurtleGraphics/Turtle_Graphic.Designer.cs
+++ M_CSHARP_TurtleGraphics/M_CSHARP_TurtleGraphics/Turtle_Graphic.Designer.cs
@@ -171,6 +171,7 @@
             // 
             this.openFileDialog1.AutoUpgradeEnabled = false;
             this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "|*.txt";
             this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
             // 
             // Turtle_Graphic
